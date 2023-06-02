b1a88b0dce3d469745b167fa74e0bcb4016be768
Dipak Firake <dipakfirake9767@gmail.com>
2023-06-02 13:29:35 +0530

    second commit

--- src/Redux/Reducers.js
+++ src/Redux/Reducers.js
@@ -2,6 +2,7 @@ import { demo } from "../demoData";
 
 
 // initializing state with some sample data if localStorage is empty
+localStorage.setItem('allBlogs',JSON.stringify(demo))
 const blogState = JSON.parse(localStorage.getItem('allBlogs')) ? JSON.parse(localStorage.getItem('allBlogs'))
     : demo;
 
